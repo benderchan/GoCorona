fd7786c484ba88589388b1c555abfd7ed7bb5514
Igor <ig.zab.ru@gmail.com>
2022-02-14 15:57:30 +0100

    Added animation on scroll

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ document.querySelector('.icon-menu').addEventListener('click', () => {
   document.querySelector('.icon-menu').classList.toggle('active')
   document.body.classList.toggle('fixed-position')
 })
+const cb = (entries, observer) => {
+  entries.forEach((el) => {
+    if (el.isIntersecting) {
+      el.target.style.opacity = 1
+      // observer.unobserve(el.target)
+    } else {
+      el.target.style.opacity = 0
+    }
+  })
+}
+const options = {
+  threshold: 0.8
+}
+const observer = new IntersectionObserver(cb, options)
+const blockTxt = document.querySelectorAll('.block-txt')
+blockTxt.forEach((el) => {
+  observer.observe(el)
+})
