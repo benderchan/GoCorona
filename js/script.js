5371f6f60afd15e2a290c5f4e5a763883f5dba5f
Igor <ig.zab.ru@gmail.com>
2022-02-14 16:41:25 +0100

    Added fade animations

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ document.querySelector('.icon-menu').addEventListener('click', () => {
   document.querySelector('.icon-menu').classList.toggle('active')
   document.body.classList.toggle('fixed-position')
 })
-const cb = (entries, observer) => {
+const textFade = (entries, observer) => {
   entries.forEach((el) => {
     if (el.isIntersecting) {
       el.target.style.opacity = 1
-      // observer.unobserve(el.target)
     } else {
       el.target.style.opacity = 0
     }
@@ -16,8 +15,17 @@ const cb = (entries, observer) => {
 const options = {
   threshold: 0.8
 }
-const observer = new IntersectionObserver(cb, options)
+const observer = new IntersectionObserver(textFade, options)
 const blockTxt = document.querySelectorAll('.block-txt')
 blockTxt.forEach((el) => {
   observer.observe(el)
 })
+const cardAnimation = (entries, observer) => {
+  entries.forEach((el) => {
+    if (el.isIntersecting) {
+      el.target.classList.add('drop')
+    }
+  })
+}
+const cards = document.querySelectorAll('.cards-healthcare__cards')
+const cardObserver = new IntersectionObserver(cardAnimation, options)
